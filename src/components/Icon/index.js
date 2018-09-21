6a02ae33a34459a4596e2f4a12fe24bfb375342d
renanpvaz <renan.piresvz@gmail.com>
2018-09-21 18:34:49 -0300

    Improve layout structure

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -3,10 +3,12 @@ import feather from 'feather-icons'
 
 import './icon.scss'
 
-const Icon = ({ name }) => (
+const getIconContent = (name, options) => ({__html: feather.icons[name].toSvg(options)})
+
+const Icon = ({ name, width = 16, height = 16 }) => (
   <i
     className="icon"
-    dangerouslySetInnerHTML={{__html: feather.icons[name].toSvg({ height: 16, width: 16 })}}
+    dangerouslySetInnerHTML={getIconContent(name, { width, height })}
   />
 )
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,13 +2,21 @@ import React from 'react'
 
 import Sidebar from '../Sidebar'
 import Container from '../Container'
+import Icon from '../Icon'
 
 import './layout.scss'
 
 const Layout = ({ children }) => (
   <div className="layout">
     <Sidebar />
-    <Container className="layout__container">{children}</Container>
+    <div className="layout__container">
+      <header className="layout__header">
+        <Icon name="menu" width="24" height="24" />
+      </header>
+      <Container className="layout__content">
+        {children}
+      </Container>
+    </div>
   </div>
 )
 
