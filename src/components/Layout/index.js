ec0c98374d2cd52b85ce7de7be72d767471e5c17
renanpvaz <renan.piresvz@gmail.com>
2018-09-24 22:22:19 -0300

    Fechar sidebar automaticamente em dispositivos pequenos

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,34 +3,61 @@ import React from 'react'
 import Sidebar from '../Sidebar'
 import Container from '../Container'
 import Icon from '../Icon'
-import { toggleSidebar } from 'store/ui/actions'
+import { toggleSidebar, closeSidebar } from 'store/ui/actions'
+import throttle from 'utils/throttle'
+
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import './layout.scss'
 
-const Layout = ({ children, toggleSidebar }) => (
-  <div className="layout">
-    <Sidebar />
-    <div className="layout__container">
-      <header className="layout__header">
-        <Icon
-          name="menu"
-          width={24}
-          height={24}
-          onClick={toggleSidebar}
-        />
-      </header>
-      <Container className="layout__content">
-        {children}
-      </Container>
-    </div>
-  </div>
-)
+class Layout extends React.Component {
+  closeSidebarIfNecessary = () => {
+    if (window.innerWidth < 860) {
+      this.props.closeSidebar()
+    }
+  }
+
+  componentDidMount() {
+    this.closeSidebarIfNecessary()
+    
+    window.addEventListener('resize',
+      throttle(300, this.closeSidebarIfNecessary)
+    )
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize')
+  }
+
+  render() {
+    const { children, toggleSidebar } = this.props
+
+    return (
+      <div className="layout">
+        <Sidebar />
+        <div className="layout__container">
+          <header className="layout__header">
+            <Icon
+              name="menu"
+              width={24}
+              height={24}
+              onClick={toggleSidebar}
+            />
+          </header>
+          <Container className="layout__content">
+            {children}
+          </Container>
+        </div>
+      </div>
+    )
+
+  }
+}
 
 export default
   connect(
     state => ({ router: state.router }),
-    dispatch => bindActionCreators({ toggleSidebar }, dispatch)
+    dispatch => bindActionCreators({ toggleSidebar, closeSidebar }, dispatch)
   )
 (Layout)
