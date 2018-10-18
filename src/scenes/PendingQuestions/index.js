1b2f54c11ed2605a3f4c52a6320013f695c7715e
Guilherme Henrique Draghetti <guilherme.draghetti@acad.pucrs.br>
2018-10-18 20:20:06 -0300

    adding pending questions screen

---
+++ src/scenes/PendingQuestions/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { push } from 'connected-react-router'
+import Card from 'components/Card'
+
+import { growl } from 'store/ui/actions'
+
+import { fetchQuestions } from 'store/question/actions'
+import { getQuestions } from 'store/question'
+import Button from 'components/Button'
+import AvailableQuestions from 'components/AvailableQuestions'
+import './pendingQuestions.scss'
+
+class PendingQuestions extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  async componentDidMount() {
+    await this.props.fetchQuestions(1, 1, 0, 0)
+  }
+
+  render() {
+    return (
+      <div className="flex flex-column">
+        <h1 className="flex">Questões pendentes</h1>
+        <div className="flex justify-between ">
+          <h6 className="listQuestios__h6">Enunciado</h6>
+          <h6 className="listQuestios__h6">Data de criação</h6>
+        </div>
+        <div className="flex flex-column">
+          <AvailableQuestions data={this.props.questions} className="flex" />
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  questions: getQuestions(state),
+})
+
+export default connect(
+  mapStateToProps,
+  dispatch =>
+    bindActionCreators(
+      {
+        fetchQuestions,
+        push,
+      },
+      dispatch,
+    ),
+)(PendingQuestions)
