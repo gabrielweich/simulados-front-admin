094bd0b5161ea13be8eaa24792ae2d90f37486f5
Eduardo <e_dornelles1992@hotmail.com>
2018-10-29 22:10:46 -0200

    removed moment dependency

--- src/components/AvailableQuestions/index.js
+++ src/components/AvailableQuestions/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Moment from 'react-moment'
 import { push } from 'connected-react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
@@ -16,9 +15,7 @@ const AvailableQuestions = props =>
       key={question.id}
     >
       <h6 className="cardQuestion__h6 ">{question.statement}</h6>
-      <h6>
-        <Moment format="DD/MM/YYYY">{question.created_at}</Moment>
-      </h6>
+      <h6>{new Date(question.created_at).toLocaleDateString()}</h6>
     </Card>
   ))
 
